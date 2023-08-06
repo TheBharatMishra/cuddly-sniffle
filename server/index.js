7c057d1cccf8e75b1837ff41920ba40b61840cd7
Bharat Mishra <mishrabm2004@gmail.com>
2023-08-06 21:59:50 +0530

    Backend

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+	user: 'mysql',
+	host: 'localhost',
+	password: 'bhanu don',
+	database: 'userLooser'
+});
+
+app.post('/create', (req, res) => {
+	console.log(req.body);
+	const name = req.body.name;
+	const age = req.body.age;
+	const country = req.body.country;
+	const position = req.body.position;
+	const wage = req.body.wage;
+	db.query(
+		'INSERT INTO Hooman (name,age,country,position,wage) VALUES (?,?,?,?,?)',
+		[name, age, country, position, wage],
+		(err, result) => {
+			if (err) {
+				console.log(err);
+			} else res.send('Values Inserted');
+		}
+	);
+});
+
+app.use('/', (req, res) => res.send());
+app.listen(6969, () => {
+	console.log('Yo mr white');
+});
