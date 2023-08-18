78c962d93c779173e4c16888041a51cd8baef9ff
TheBharatMishra <mishrabm2004@gmail.com>
2023-08-18 23:56:46 +0530

    Updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,62 +1,96 @@
-import { useState } from 'react';
-import axios from 'axios';
-
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-	const [name, setName] = useState('');
-	const [age, setAge] = useState(0);
-	const [country, setCountry] = useState('');
-	const [position, setPosition] = useState('');
-	const [wage, setWage] = useState(0);
-	const addHooman = () => {
-		console.log(name);
-		axios
-			.post('https://127.0.0.1:6969/create', {
-				name: name,
-				age: age,
-				country: country,
-				position: position,
-				wage: wage
-			})
-			.then(() => console.log('Success'))
-			.catch((error) => console.log(error));
-	};
+  const [hoomanList, setHoomanList] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+  const getHooman = () => {
+    axios
+      .get("http://127.0.0.1:6969/hooman")
+      .then((response) => setHoomanList(response.data));
+  };
+  const deleteHooman = (hoomanName: any) => {
+    axios
+      .delete("http://127.0.0.1:6969/gone/", { name: name })
+      .then((hoomanName) => {
+        // setHoomanList(hoomanList.filter((user) => (user.empName = hoomanName)));
+      });
+  };
+  const addHooman = () => {
+    console.log(name);
+    axios
+      .post("http://127.0.0.1:6969/create", {
+        name: name,
+        age: age,
+        country: country,
+        position: position,
+        wage: wage,
+      })
+      .then(() =>
+        setHoomanList([
+          ...getHooman,
+          {
+            name: name,
+            age: age,
+            country: country,
+            position: position,
+            wage: wage,
+          },
+        ])
+      )
+      .catch((error) => console.log(error));
+  };
 
-	return (
-		<>
-			<div>
-				<h3>Name: {name}</h3>
-				<input
-					onChange={(e) => setName(e.target.value)}
-					type='text'
-				/>
-				<h3>Age: {age}</h3>
-				<input
-					onChange={(e) => setAge(e.target.value)}
-					type='number'
-				/>
-				<h3>Country: {country}</h3>
-				<input
-					onChange={(e) => setCountry(e.target.value)}
-					type='text'
-				/>
-				<h3>Position: {position}</h3>
-				<input
-					onChange={(e) => setPosition(e.target.value)}
-					type='text'
-				/>
-				<h3>Wage: {wage}</h3>
-				<input
-					onChange={(e) => setWage(e.target.value)}
-					type='number'
-				/>
-			</div>
-			<div>
-				<button onClick={addHooman}>Submit</button>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="bg-gray-500 font-mono p-10 flex-col text-center m-5 rounded-lg  ">
+        <h3>Name: {name}</h3>
+        <input onChange={(e) => setName(e.target.value)} type="text" />
+        <h3>Age: {age}</h3>
+        <input onChange={(e) => setAge(e.target.value)} type="number" />
+        <h3>Country: {country}</h3>
+        <input onChange={(e) => setCountry(e.target.value)} type="text" />
+        <h3>Position: {position}</h3>
+        <input onChange={(e) => setPosition(e.target.value)} type="text" />
+        <h3>Wage: {wage}</h3>
+        <input onChange={(e) => setWage(e.target.value)} type="number" />
+        <br />
+        <button
+          className="rounded-full bg-slate-600 p-2 center"
+          onClick={addHooman}
+        >
+          Submit
+        </button>
+      </div>
+
+      <hr />
+
+      <div className="bg-red-400 font-mono p-10 flex-col text-center">
+        <button
+          className="rounded-full bg-slate-600 p-2 center"
+          onClick={getHooman}
+        >
+          Show Hooman
+        </button>
+        {hoomanList.map((val: any, key: any) => {
+          return (
+            <div className="m-10 flex justify-center text-center flex-col bg-gray-300 rounded-lg">
+              <h3 key={key.empName}>Name: {val.empName}</h3>
+              <h3 key={key.empAge}>Age: {val.empAge}</h3>
+              <h3 key={key.empWage}>Wage: {val.empWage}</h3>
+              <h3 key={key.empPosition}>Position: {val.empPosition}</h3>
+              <h3 key={key.empCountry}>Country: {val.empCountry}</h3>
+              <button onClick={() => deleteHooman(val.empName)}>DELETE</button>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,37 +1,62 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql');
-const cors = require('cors');
+const mysql = require("mysql2");
+const cors = require("cors");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 const db = mysql.createConnection({
-	user: 'root',
-	host: '127.0.0.1',
-	password: 'bhanu don',
-	database: 'userLooser'
+  user: "root",
+  host: "127.0.0.1",
+  password: "password",
+  database: "userLooser",
 });
 
-app.post('/create', (req, res) => {
-	console.log(req.body);
-	const name = req.body.name;
-	const age = req.body.age;
-	const country = req.body.country;
-	const position = req.body.position;
-	const wage = req.body.wage;
-	db.query(
-		'INSERT INTO Hooman (name,age,country,position,wage) VALUES (?,?,?,?,?)',
-		[name, age, country, position, wage],
-		(err, result) => {
-			if (err) {
-				console.log(err);
-			} else res.send('Values Inserted');
-		}
-	);
-});
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
 
-app.use('/', (req, res) => res.send('Wassup'));
+  db.query(
+    "INSERT INTO employees (empName,empAge,empCountry,empPosition,empWage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else res.send("Values Inserted");
+    }
+  );
+});
+app.get("/hooman", (req, res) => {
+  db.query("SELECT * from employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else res.send(result);
+  });
+});
+app.delete("/gone", (req, res) =>
+  db.query(
+    `DELETE FROM employees WHERE empName='${req.body.name}'`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else res.send(result);
+    }
+  )
+);
+
+// app.use("/", (req, res) => res.send("Wassup"));
 app.listen(6969, () => {
-	console.log('Yo mr white');
+  console.log("Yo mr white");
 });
